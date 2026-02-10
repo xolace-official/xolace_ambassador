94e844a8a62b96e7bb4c3e04d57a86b67bc58015
Emmanuel <emmsom506@gmail.com>
2026-02-10 01:16:59 +0000

    Chore page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,19 @@ import JoinProgramForm from '@/components/ambassador/join-program';
 import ProgramDetails from '@/components/ambassador/program-details';
 import Benefits from '@/components/ambassador/benefits';
 import Footer from '@/components/ambassador/footer';
+import NavBar from "@/components/layout/nav-bar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background text-foreground">
-      <HeroSection />
-      <JoinProgramForm />
-      <ProgramDetails />
-      <Benefits />
-      <Footer />
-    </div>
+    <main className={"flex items-start justify-start w-full min-h-screen bg-background text-foreground"}>
+      <div className="flex items-start justify-start w-full flex-col ">
+        {/*<NavBar/>*/}
+        <HeroSection/>
+        <JoinProgramForm/>
+        <ProgramDetails/>
+        <Benefits/>
+        {/*<Footer/>*/}
+      </div>
+    </main>
   );
 }
